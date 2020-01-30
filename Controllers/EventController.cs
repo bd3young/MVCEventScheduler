9638b42698edbdec89a67228e3fb78312eb40832
beaudeyoung <beau@beaudeyoung.com>
2020-01-30 15:14:40 -0500

    push 4

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,5 +1,9 @@
 ﻿using System.Web.Mvc;
 using MVCEventScheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace MVCEventScheduler.Controllers
 {
@@ -8,21 +12,51 @@ namespace MVCEventScheduler.Controllers
         // GET: Event
 
         [HttpPost]
-        public ActionResult NewEvent(string eventName, string eventHost, string email)
+        public ActionResult NewEvent(string eventName, string eventType, string eventHost, string email, DateTime eventDateTime, int id, bool isPublic)
         {
             EventModel e = new EventModel()
             {
+                EventName = eventName,
+                EventType = eventType,
+                EventHost = eventHost,
+                Email = email,
+                EventDateTime = eventDateTime,
+                Id = id,
+                IsPublic = isPublic
 
             };
-            ViewBag.Message = $"Name: {eventName}";
-            ViewBag.Message = $"Host: {eventHost}";
-            ViewBag.Message += $"email: {email}";
-            return View("ConfirmEvent");
+
+            if (isPublic == true)
+            {
+                //ViewBag.Message += $"Name: {eventName}    ";              
+                //ViewBag.Message += $"Host: {eventHost}    ";
+                //ViewBag.Message += $"Email: {email}    ";
+                //ViewBag.Message += $"Public: {isPublic}    ";
+                return View("ConfirmPublicEvent", e);
+            }
+            else
+            {
+                //ViewBag.Message += $"Name: {eventName}    ";
+                //ViewBag.Message += $"Host: {eventHost}    ";
+                //ViewBag.Message += $"Email: {email}    ";
+                //ViewBag.Message += $"Public: {isPublic}    ";
+                return View("ConfirmEvent", e);
+            }
         }
 
         public ActionResult NewEvent()
         {
             return View("NewEventForm");
+        }
+
+        public ActionResult EventCalc()
+        {
+            return View("EventCalculator");
+        }
+
+        public ActionResult PostedEvents()
+        {
+            return View("PostedEventsForm");
         }
     }
 }
